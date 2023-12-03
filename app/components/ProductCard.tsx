ee8364998a018d7bf48b6547df40582cabdeb51d
Gabobe15 <gabobe33@gmail.com>
2023-12-03 19:42:56 +0300

    next js with tailwing first commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCard from './AddToCard'
+
+
+const ProductCard = () => {
+  return (
+    <div className='text-blue-800' >
+        <AddToCard/>
+    </div>
+  )
+}
+
+export default ProductCard
